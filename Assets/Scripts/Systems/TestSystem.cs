f942513665716d614fd8386541a42aafc345836a
Max <maxzhmach@mail.ru>
2024-04-02 00:11:51 +0300

    Init commit

---
+++ Assets/Scripts/Systems/TestSystem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Leopotam.EcsLite;
+using UnityEngine;
+
+namespace Dune.IO
+{
+    public class TestSystem : IEcsRunSystem, IEcsInitSystem
+    {
+        
+        
+        public void Init(IEcsSystems systems)
+        {
+            Debug.Log("Init");
+        }
+        
+        
+        public void Run(IEcsSystems systems)
+        {
+            
+        }
+        
+    }
+}
